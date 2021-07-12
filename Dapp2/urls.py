478f907e2f7dc9e5189ae7dff0b330ded1344bf6
vishnuvs1993 <vishnuvs8476@gmail.com>
2021-07-12 14:28:03 +0530

    update2

--- Dapp2/urls.py
+++ Dapp2/urls.py
@@ -3,5 +3,15 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
    path('vs1',views.vs),
-   path('fun1',views.fuc)
+   path('login',views.fuc),
+   path('home',views.home1),
+   path('about',views.about1),
+   path('products',views.products1),
+   path('services',views.services1),
+   path('msi gaming s1',views.msi1),
+   path('msi gaming s2',views.msi2),
+   path('msi gaming s3',views.msi3),
+   
+   path('msi gaming s5',views.msi5),
+   path('contact us',views.contactus1)
 ]
--- Dapp2/views.py
+++ Dapp2/views.py
@@ -6,4 +6,24 @@ from django.http import HttpResponse
 def vs(request):
     return HttpResponse("hello world")
 def fuc(request):
-    return render(request,'test.html')    
+    return render(request,'login.html')    
+def home1(request):
+    return render(request,'home.html') 
+def about1(request):
+    return render(request,'about.html')  
+def products1(request):
+    return render(request,'products.html')
+def services1(request):
+    return render(request,'services.html')
+def msi1(request):
+    return render(request,'msi gaming s1.html')
+def msi2(request):
+    return render(request,'msi gaming s2.html')    
+def msi3(request):
+    return render(request,'msi gaming s3.html')
+
+def msi5(request):
+    return render(request,'msi gaming s5.html') 
+def contactus1(request):
+    return render(request,'contact us.html')           
+              
